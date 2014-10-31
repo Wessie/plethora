422ab4388360f80f81db292ef3e729a4d274ca21
wessie <github@wessie.info>
2014-10-31 16:24:23 +0100

    Added tests for TestConfiguration

--- config/location_test.go
+++ config/location_test.go
@@ -1,6 +1,10 @@
 package config
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestConfigInit(t *testing.T) {
 	defer TestConfiguration()()
@@ -21,5 +25,40 @@ func TestConfigInit(t *testing.T) {
 
 	if Location() != newLoc {
 		t.Error("failed: location is not updated")
+	}
+}
+
+func TestTestingConfiguration(t *testing.T) {
+	loc := dbLocation
+
+	cleanup := TestConfiguration()
+
+	if loc.filename == dbLocation.filename {
+		t.Error("test location is empty string, instead of temporary filepath")
+	}
+
+	if loc.path == dbLocation.path {
+		t.Error("test database location is empty string, instead of temporary directory")
+	}
+
+	f, err := os.Create(dbLocation.filename)
+	if err != nil {
+		t.Fatal("failed to create file in temporary directory", err)
+	}
+	f.Close()
+
+	// keep a copy around for after cleanup
+	filename := dbLocation.filename
+	// check if the cleanup function properly deletes everything
+	cleanup()
+
+	_, err = os.Stat(filename)
+	if !os.IsNotExist(err) {
+		t.Error("cleanup did not remove temporary file")
+	}
+
+	_, err = os.Stat(filepath.Dir(filename))
+	if !os.IsNotExist(err) {
+		t.Error("cleanup did not remove temporary directory")
 	}
 }
