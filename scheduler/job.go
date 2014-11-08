e52ec28f6388f5e3301b3e537c574e9476bdb457
wessie <github@wessie.info>
2014-11-08 19:47:27 +0100

    Added String method to Task type
    The String method tries to use Job and Planner as fmt.Stringer if
    implemented, and otherwise uses reflection to generate a name.

--- scheduler/job.go
+++ scheduler/job.go
@@ -1,5 +1,10 @@
 package scheduler
 
+import (
+	"reflect"
+	"runtime"
+)
+
 // Job is a runnable job to be run by the scheduler
 type Job interface {
 	Run() error
@@ -11,4 +16,8 @@ type FuncJob func() error
 
 func (fn FuncJob) Run() error {
 	return fn()
+}
+
+func (fn FuncJob) String() string {
+	return runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
 }
--- scheduler/task.go
+++ scheduler/task.go
@@ -1,5 +1,10 @@
 package scheduler
 
+import (
+	"fmt"
+	"reflect"
+)
+
 // Task is a task to be ran by the scheduler, it requires
 // a Job to execute, and a Planner to plan said execution.
 type Task struct {
@@ -20,4 +25,19 @@ func (t Task) Cancel() error {
 
 	t.schedule.removeTask <- t
 	return nil
+}
+
+func (t Task) String() string {
+	var j, p fmt.Stringer
+	var ok bool
+
+	if j, ok = t.Job.(fmt.Stringer); !ok {
+		j = reflect.TypeOf(t.Job)
+	}
+
+	if p, ok = t.Planner.(fmt.Stringer); !ok {
+		p = reflect.TypeOf(t.Planner)
+	}
+
+	return fmt.Sprintf("%s/%s", j, p)
 }
